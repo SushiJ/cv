474714d66921d44ca2b852618816548c5a99313a
Sushant Sharma <sushant.wurk@gmail.com>
2024-07-19 23:22:40 +0530

    update data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,9 +80,7 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
-                      <a className="hover:underline" href={work.link}>
-                        {work.company}
-                      </a>
+                      <p className="hover:underline">{work.company}</p>
 
                       <span className="inline-flex gap-x-1">
                         {work.badges.map((badge) => (
